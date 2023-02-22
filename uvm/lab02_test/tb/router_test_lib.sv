726da3b258dc6551f599a346f5a9894fb31d477c
moses <moses@localhost.localdomain>
2023-02-22 10:18:31 +0300

    added: fixed errors -- correct name in uvm_info and funtcion labels

--- uvm/lab02_test/tb/router_test_lib.sv
+++ uvm/lab02_test/tb/router_test_lib.sv
@@ -37,9 +37,10 @@ function void base_test::build_phase(uvm_phase phase);
     /*  note: Do not call super.build_phase() from any class that is extended from an UVM base class!  */
     /*  For more information see UVM Cookbook v1800.2 p.503  */
     super.build_phase(phase);
-    `uvm_info("REPORT", "build phase in testbench router_tb has been executed", UVM_HIGH)
+    `uvm_info("REPORT", "build phase in test base test has been executed", UVM_HIGH)
 endfunction: build_phase
 
 function void base_test::end_of_elaboration_phase(uvm_phase phase);
     uvm_top.print_topology(); 
-endfunction: build_phase
+    `uvm_info("REPORT", "end_of_elaboration_phase in test base test has been executed", UVM_HIGH)
+endfunction: end_of_elaboration_phase
